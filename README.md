239933f1457e0921cde1ed0c87daeaf0a4ddbb83
briiiqtt <briiiqtt@gmail.com>
2026-01-14 02:00:34 +0900

    add backend springboot settings

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 - 단축 URL 생성 및 리다이렉트 서비스 
 
+# 실행 방법
+## 개발
+```
+docker compose -f compose.dev.yml up
+```
+## 배포 (배포시 추가예정)
+```
+docker compose -f compose.prod.yml up --build 
+```
+
+
 ## 주요 기능
 
 ### 단축 URL 생성
---
+++ backend/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "dev.briiiqtt"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    //	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    //	runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
