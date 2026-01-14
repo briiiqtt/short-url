0762116987e647611b96d0676032993bb1c178b4
briiiqtt <briiiqtt@gmail.com>
2026-01-14 23:32:52 +0900

    add Redis feature

--- backend/src/main/java/dev/briiiqtt/shorturl/url/service/UrlService.java
+++ backend/src/main/java/dev/briiiqtt/shorturl/url/service/UrlService.java
@@ -1,5 +1,7 @@
 package dev.briiiqtt.shorturl.url.service;
 
+import dev.briiiqtt.shorturl.cache.UrlCache;
+import dev.briiiqtt.shorturl.cache.UrlCacheService;
 import dev.briiiqtt.shorturl.url.domain.UrlEntity;
 import dev.briiiqtt.shorturl.url.domain.UrlRepository;
 import dev.briiiqtt.shorturl.util.Base62;
@@ -12,16 +14,25 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UrlService {
     private final UrlRepository urlRepository;
+    private final UrlCacheService urlCacheService;
 
     public String createUrl(String req) {
         UrlEntity entity = new UrlEntity();
         entity.setUrl(req);
         urlRepository.save(entity);
+        urlCacheService.saveUrl(new UrlCache(entity.getId(), entity.getUrl()));
         return entity.getShortenUrl();
     }
 
     public String readUrl(String req) {
-        Optional<UrlEntity> o = urlRepository.findById(Base62.decode(req));
+        long id = Base62.decode(req);
+
+        UrlCache urlCache = urlCacheService.getUrl(id);
+        if (urlCache != null) {
+            return urlCache.getUrl();
+        }
+
+        Optional<UrlEntity> o = urlRepository.findById(id);
         if (o.isEmpty()) return null;
 
         UrlEntity entity = o.get();
