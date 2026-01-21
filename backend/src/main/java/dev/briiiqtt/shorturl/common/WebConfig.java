c1767bf2cc61b7debb70c6c5584614c3a937b04f
briiiqtt <briiiqtt@gmail.com>
2026-01-21 22:13:28 +0900

    chore: struggling with cors..

--- backend/src/main/java/dev/briiiqtt/shorturl/common/WebConfig.java
+++ backend/src/main/java/dev/briiiqtt/shorturl/common/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5173", "https://shorturl.bombs.kr")
+                .allowedOrigins("http://127.0.0.1:5173", "https://shorturl.bombs.kr", "https://short.bombs.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
