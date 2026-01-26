7614ff7708c89e6018bb5c653c3920d8627f802c
briiiqtt <briiiqtt@gmail.com>
2026-01-26 18:11:02 +0900

    chore: refactor frontend layout directory; add some todo comments

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
-// components/Layout.js
 import { Outlet } from 'react-router-dom';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
 
 const Layout = () => {
   return (
