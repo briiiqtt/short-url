8929e05954331574a56693fb2d02cd9670ccee2d
briiiqtt <briiiqtt@gmail.com>
2026-02-01 02:02:02 +0900

    feat: configure flyway; add healthcheck on backend; make front depend on backend

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 tasks.withType<Test> {
